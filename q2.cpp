6c23412f3672c2e92e38d6cd930275a46f17dbe7
singhnidhi0301 <31719235+singhnidhi0301@users.noreply.github.com>
2017-09-07 09:49:36 +0530

    Add files via upload

---
+++ q2.cpp
@@ -0,0 +1,30 @@
+//Program to find indices i,j,k which satisfy the equation:- A[i]^2+A[j]^2=A[k]^2
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	int a[50],i,j,k,pos1,pos2,pos3,n,sum=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<n-2;i++)
+	{
+		for(j=i+!;j<n-1;j++)
+		{	
+			for(k=j+1;j<n;j++)
+			{
+				sum+=((a[i]*a[i])+(a[j]*a[j]));
+				if(sum==(a[k]*a[k]))
+				{
+					pos1=i;
+					pos2=j;
+					pos3=k;
+				}
+			}
+		}
+	}
+	printf("Indices are %d %d %d ".a[pos1],a[pos2],a[pos3]);
+	getch();
+	return 0;
+}
